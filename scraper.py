41beb6e605133af5fbd9859fbaa46e191267aef7
Ethan Woolley <adama@polaris.bigdipper>
2020-05-06 13:37:17 -0700

    added scraper

--- scraper.py
+++ scraper.py
@@ -3,75 +3,76 @@ from bs4 import BeautifulSoup as BS
 from requests import get
 from urllib.request import urlopen
 
-def get_langs():
-    soup = BS(get('https://www.churchofjesuschrist.org/languages?lang=eng').text, 'html.parser')
-    # Sort all the langauages into dict with name and 3 char shorthand for url
-    return {a.get('data-lang'): a.contents[0] for a in [li.findChild('a') for li in soup.findAll('li')]}
+class scriptureScraper():
 
-def get_testaments():
-    soup = BS(get(f'https://www.churchofjesuschrist.org/study/scriptures').text, 'html.parser')
-    # Get the testament name and url and img link for every testament
-    return {a.get('href').split('?')[0]: a.text for a in elem_with_attr(soup, 'div', ('class', 'tileRow-3ETk4'))[0].findAll('a')}
+    rootURL = 'https://www.churchofjesuschrist.org/study/scriptures'
+    langURL = 'https://www.churchofjesuschrist.org/languages'
 
+    def __init__(self):
+        pass
 
-#def get_testaments(lang='eng'):
-#    soup = BS(get(f'https://www.churchofjesuschrist.org/study/scriptures?lang={lang}').text, 'html.parser')
-#    # Get the testament name and url and img link for every testament
-#    return {a.text: (a.get('href').split('?')[0], a.find('img').get('src')) for a in elem_with_attr(soup, 'div', ('class', 'tileRow-3ETk4'))[0].findAll('a')}
+    def get_langs(self):
+        soup = BS(urlopen(self.langURL).read(), 'html.parser')
+        langs = [li.find('a') for li in soup.findAll('li')]
+        return {a.get('data-lang') : a.text for a in langs}
 
-#def get_testaments2(lang, testament):
-#    soup = BS(get(f'https://churchofjesuschrist.org{testament}?lang={lang}').text, 'html.parser')
-#    # Get the book name and chapters names with url to each chapter
-#    return {book.find('a').text: [(chptr.text, chptr.find('a').get('href')) for chptr in book.findAll('li')] for book in nav.elem_with_attr(soup, 'nav', ('class', 'tableOfContents-3u3H3'))[0].find('ul').findAll('li')}
+    def get_books(self, lang):
+        soup = BS(urlopen(self.rootURL+f'?lang={lang}'), 'html.parser')
+        books = [ a for a in soup.find('section').find('section').findAll('a') ] 
+        return { a.get('href').split('/')[-1].split('?')[0] : a.find('span').text for a in books}
 
-def elem_with_attr(soup, elem, attr):
-    return [elm for elm in soup.findAll(elem) if elm.has_attr(attr[0]) and attr[1] in elm.get(attr[0])]
+    def get_gospels(self, lang, book):
+        soup = BS(urlopen(self.rootURL+f'/{book}?lang={lang}'), 'html.parser')
+        As = [a for a in soup.find('nav', {'class': 'manifest'}).findAll('a')]
+        gospels = {}
+        for a in As:
+            try:
+                int(a.get('href').split('/')[-1].split('?')[0])
+                gospels[a.get('href').split('/')[-2]] = a.text
+            except ValueError:
+                gospels[a.get('href').split('/')[-1].split('?')[0]] = a.text
+        return gospels
 
-def get_books_from_testament(testament,lang='eng'):
+    def get_gospels(self, lang, book):
 
-    options = webdriver.FirefoxOptions()
-    options.binary_location = "/usr/bin/firefox"
-    driver = webdriver.Firefox(options=options)
-    driver.get(f'https://churchofjesuschrist.org{testament}?lang={lang}')
-    svgs = [svg for svg in driver.find_elements_by_tag_name('svg') if svg.get_attribute('style') == 'width: 1em; height: 1em;']#[1:]
-    [svg.click() for svg in svgs]
+        options = webdriver.FirefoxOptions()
+        options.binary_location = "/usr/bin/firefox"
+        driver = webdriver.Firefox(options=options)
+        driver.get(self.rootURL+f'/{book}?lang={lang}')
+        svgs = [svg for svg in driver.find_elements_by_tag_name('svg') if svg.get_attribute('style') == 'width: 1em; height: 1em;']#[1:]
+        [svg.click() for svg in svgs]
 
-    soup = BS(driver.page_source, 'html.parser')
-    nav = elem_with_attr(soup, 'nav', ('class', 'tableOfContents-3u3H3'))[0]
-    books = {}
+        soup = BS(driver.page_source, 'html.parser')
+        nav = soup.find('nav', {'class': 'tableOfContents-3u3H3'})
+        books = {}
 
-    for child in nav.find('ul').children:
-        try:
-            if child.name == 'li':
-                if child.find('ul'):
-                    chapters = child.find('ul').findAll('a')
-                    books[child.find('a').text.strip()] = (chapters[0].get('href').split('?')[0].split('/')[-2], str(len(chapters)))
-                else:
-                    books[child.text.strip()] = (child.find('a').get('href').split('?')[0].split('/')[-1], '1')
-        except:
-            pass
+        for child in nav.find('ul').children:
+            try:
+                if child.name == 'li':
+                    if child.find('ul'):
+                        chapters = child.find('ul').findAll('a')
+                        books[child.find('a').text.strip()] = (chapters[0].get('href').split('?')[0].split('/')[-2], str(len(chapters)))
+                    else:
+                        books[child.text.strip()] = (child.find('a').get('href').split('?')[0].split('/')[-1],)
+            except:
+                pass
 
-        #        for chapter in a.find_next_sibling().findAll('a'):
-        ###            print(chapter)
-        #            if chapter.has_attr('href'):
-        #                books[a.text].append(chapter.get('href').split('?')[0])
+        driver.close()
 
-    return books
+        return {book : books[book] for book in list(books.keys())[1:]}
 
-     #books = {a.text: [(chapter.text, chapter.get('href').split('?')[0]) for chapter in a.find_next_sibling().findAll('a')] for a in nav}
-
-def get_chapter(testament, book, chapter, lang):
-    try:
-        url = f'https://churchofjesuschrist.org{testament}/{book}/{chapter}?lang={lang}'
-        html = urlopen(url).read()
-        soup = BS(html, 'html.parser')
-        return [p.text for p in elem_with_attr(soup, 'div', ('class', 'body'))[0].findAll('p')]
-    except:
-        return ["Sorry, but we do not have that chapter in that language"]
+    def get_chapters(self, lang, book, gospel, chapter):
+        soup = BS(urlopen(self.rootURL+f'/{book}/{gospel}/{chapter}?lang={lang}'), 'html.parser')
+        summary = soup.find('p', {'id': 'study_summary1'}).text
+        intro = soup.find('p', {'id': 'intro1'}).text if soup.find('p', {'id': 'intro1'}) else None
+        comprising = soup.find('p', {'id': 'study_intro1'}).text if soup.find('p', {'id': 'study_intro1'}) else None
+        verses = [p.text for p in soup.find('div', {'class': 'body-block'}).findAll('p')]
+        chapter = {'intro': intro,
+                    'comprinsing': comprising,
+                    'summary': summary,
+                    'veres': verses}
+        return chapter
 
 if __name__ == "__main__":
-    from info import testaments
-    books = {}
-    for testament, name in testaments.items():
-        books[name] = get_books_from_testament(testament)
-    print(books)
+    scraper = scriptureScraper()
+    print(scraper.get_chapters('eng', 'bofm', 'alma', '9'))
